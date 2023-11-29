762054e8a74bc23ee50b90428e05d3535a0a4b14
nimmy-george <122686221+nimmy-george@users.noreply.github.com>
2023-11-28 21:01:44 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,27 +1,25 @@
-# tests/test_app.py
-
 import pytest
-from app.app import app, init_db
+from app import app, init_db
 
 @pytest.fixture
 def client():
-    test_app = app
-    test_app.config['TESTING'] = True
-    with test_app.test_client() as client:
-        # Initialize the database before each test
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    # Initialize the test database
+    with app.app_context():
         init_db()
-        yield client
+
+    yield client
 
 def test_home_route(client):
     response = client.get('/')
-    assert b'User List' in response.data
+    assert b'Welcome to the Home Page' in response.data
 
 def test_add_user(client):
-    # Assuming you have an add_user route
     response = client.post('/add_user', data={'username': 'Nimmy', 'email': 'c0872805@mylambton.ca'})
     assert response.status_code == 302  # Assuming a redirect after adding a user
-
+    
     # Verify that the user is added by checking the updated user list
     response = client.get('/')
     assert b'Nimmy' in response.data
-    assert b'c0872805@mylambton.ca' in response.data
