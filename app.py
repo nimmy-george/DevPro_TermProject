7d718346921dac10fd1b18ae1c5cc9e8aea4ca91
nimmy-george <122686221+nimmy-george@users.noreply.github.com>
2023-12-05 17:50:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy.model import Model
+from typing import List
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Use the same SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -11,24 +10,21 @@ class User(db.Model):
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
 
-# Database initialization
 def init_db() -> None:
-    with app.app_context():  # Set up the application context
+    with app.app_context():
         db.create_all()
 
-# Route for the home page
 @app.route('/')
-def home():
-    users = User.query.all()
+def home() -> str:
+    users: List[User] = User.query.all()
     return render_template('index.html', users=users)
 
-# Route for adding a new user
 @app.route('/add_user', methods=['POST'])
-def add_user():
-    username = request.form['username']
-    email = request.form['email']
+def add_user() -> str:
+    username: str = request.form['username']
+    email: str = request.form['email']
 
-    new_user = User(username=username, email=email)
+    new_user: User = User(username=username, email=email)
     db.session.add(new_user)
     db.session.commit()
 
@@ -36,5 +32,4 @@ def add_user():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=False, port=4000)
-
+    app.run(debug=True, port=4000)
