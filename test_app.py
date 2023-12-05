fca1b133424cfc5b7d5cecb49f32c5e98fc88253
nimmy-george <122686221+nimmy-george@users.noreply.github.com>
2023-12-05 17:51:17 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,41 +1,27 @@
-import unittest
 from app import app, db, User
+import unittest
 
 class TestApp(unittest.TestCase):
-
-    def setUp(self):
-        # Use an SQLite in-memory database for testing
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    def setUp(self) -> None:
         app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
         self.app = app.test_client()
+        db.create_all()
 
-        with app.app_context():  # Set up the application context
-            db.create_all()
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
 
-    def tearDown(self):
-        with app.app_context():  # Set up the application context
-            db.session.remove()
-            db.drop_all()
-
-    def test_home_page(self):
+    def test_home_page(self) -> None:
         response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'User List', response.data)
+        self.assertIn(b'Member List', response.data)
 
-    def test_add_user(self):
-        response = self.app.post('/add_user', data={'username': 'testuser', 'email': 'testuser@example.com'})
-        self.assertEqual(response.status_code, 302)  # 302 is the HTTP status code for a redirect
-
-        # Check if the user was added to the database
-        with app.app_context():  # Set up the application context
-            user = User.query.filter_by(username='testuser').first()
-            self.assertIsNotNone(user)
-            self.assertEqual(user.username, 'testuser')
-            self.assertEqual(user.email, 'testuser@example.com')
-
-        # Check if the user is displayed on the home page
-        response = self.app.get('/')
-        self.assertIn(b'testuser - testuser@example.com', response.data)
+    def test_add_user(self) -> None:
+        response = self.app.post('/add_user', data=dict(
+            username='testuser',
+            email='testuser@example.com'
+        ), follow_redirects=True)
+        self.assertIn(b'testuser', response.data)
 
 if __name__ == '__main__':
     unittest.main()
