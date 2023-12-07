df6888b4ff8ffea06f902ebdf76fc5428e48f6f6
nimmy-george <122686221+nimmy-george@users.noreply.github.com>
2023-12-06 23:12:32 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -26,7 +26,18 @@ class TestApp(unittest.TestCase):
 
     def test_add_user(self):
         response = self.app.post('/add_user', data={'username': 'testuser', 'email': 'testuser@example.com'})
-        # Your test assertions for adding a user
+        self.assertEqual(response.status_code, 302)  # 302 is the HTTP status code for a redirect
+
+        # Check if the user was added to the database
+        user = User.query.filter_by(username='testuser').first()
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'testuser@example.com')
+
+        # Check if the user is displayed on the home page
+        response = self.app.get('/')
+        self.assertIn(b'testuser - testuser@example.com', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
